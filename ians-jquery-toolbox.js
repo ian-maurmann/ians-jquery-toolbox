8dced072073e1733e38bd415292d7b57cb0b11ea
Ian Maurmann <ikmxx@yahoo.com>
2024-05-18 12:28:57 -0700

    Create new Toolbox object

---
+++ ians-jquery-toolbox.js
@@ -0,0 +1,7 @@
+// noinspection ES6ConvertVarToLetConst
+
+// noinspection JSUnusedAssignment
+if( typeof IansJqueryToolbox === 'undefined' || IansJqueryToolbox === null || IansJqueryToolbox === false){
+    // noinspection SpellCheckingInspection
+    var IansJqueryToolbox = {};
+}
